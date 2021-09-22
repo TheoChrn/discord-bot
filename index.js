d365cbad816fad2693f124ec3e9b8ed90a6bcdea
Theo Cheron <35193221+Volturuss@users.noreply.github.com>
2021-09-22 13:59:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,26 +13,31 @@ const client = new Client({
 })
 
 client.on('ready', () => {
-  let guild = client.guilds.cache.get('696324754520670218')
-  let testChannel = guild.channels.cache.get('754083246480031926')
-  const roleLegends = guild.roles.resolve('847504515947036763')
+  let guild = client.guilds.cache.get('677854785654882304')
+  let testChannel = guild.channels.cache.get('874050104401747998')
+  const roleLegends = guild.roles.resolve('874065885634113628')
   const messageRemider = `${roleLegends} Prière svp et cliquez sur la réaction quand c'est fait 🙏`
-  const legendsUserId = roleLegends.members.map(u => u.id)
-  const botId = '874050542433873970'
-  let newArr = []
+  //const legendsUserId = roleLegends.members.map(u => u.id)
+  //const botId = '874050542433873970'
+  //let newArr = []
 
   const testMessage = new cron.CronJob('00 20 * * *', () => {
 
     testChannel.send(messageRemider).then(messageRemider => {
-      messageRemider.react('🙏').then(() => {
-        const filter = (reaction, user) => {
+      messageRemider.react('🙏')
+    })
+  })
+  testMessage.start()
+})
+
+/*const filter = (reaction, user) => {
           return reaction.emoji.name === '🙏' && user.id !== botId
-        }
-        const collector = messageRemider.createReactionCollector({ filter, time: 7200000 })
+        }*/
+        /*const collector = messageRemider.createReactionCollector({ filter, time: 7200000 })
         collector.on('collect', (reaction, user) => {
           console.log(`${user.tag} reacted with ${reaction.emoji.name}.`)
-        })
-        collector.on('end', collection => {
+        })*/
+        /*collector.on('end', collection => {
           collection.map(reaction => {
             const reactUsers = reaction.users.cache.map(u => u.id)
             newArr = legendsUserId.filter(users => !reactUsers.includes(users))
@@ -46,11 +51,6 @@ client.on('ready', () => {
               testChannel.send(`Tout le monde a prié ! 🤗`)
             }
           })
-        })
-      })
-    })
-  })
-  testMessage.start()
-})
+        })*/
 
 client.login(process.env.TOKEN)
